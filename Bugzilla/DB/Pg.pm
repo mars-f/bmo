43ba5b0a0ed8e8750199dfa99165085ade49a30b
Dylan William Hardison <dylan@hardison.net>
2017-12-02 13:27:20 -0500

    utf8 is always on

--- Bugzilla/DB/Pg.pm
+++ Bugzilla/DB/Pg.pm
@@ -52,7 +52,7 @@ sub BUILDARGS {
     # creating tables.
     $dsn .= ";options='-c client_min_messages=warning'";
 
-    my $attrs = { pg_enable_utf8 => Bugzilla->params->{'utf8'} };
+    my $attrs = { pg_enable_utf8 => "UTF8 FOREVER" };
 
     return { dsn => $dsn, user => $user, pass => $pass, attrs => $attrs }
 }
